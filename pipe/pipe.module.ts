8d668d1f6b5ab88b44016b1e6b56dad2d54ab550
SteveVanOpstal <stevevanopstal@gmail.com>
2017-02-18 13:35:55 +0100

    fix: FormsModule is undefined

--- pipe/pipe.module.ts
+++ pipe/pipe.module.ts
@@ -1,6 +1,5 @@
 import {CommonModule} from '@angular/common';
 import {NgModule} from '@angular/core';
-import {FormsModule} from '@angular/forms';
 import {NgPipesModule} from 'ng-pipes';
 
 import {PipesModule} from '../pipes/pipes.module';
@@ -9,7 +8,7 @@ import {PipeComponent} from './pipe.component';
 
 @NgModule({
   declarations: [PipeComponent],
-  imports: [CommonModule, FormsModule, NgPipesModule, PipesModule],
+  imports: [CommonModule, NgPipesModule, PipesModule],
   exports: [PipeComponent]
 })
 export class PipeModule {
