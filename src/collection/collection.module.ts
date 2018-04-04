29290f264d2235c764c7f1f04e94585933610406
Steve Van Opstal <stevevanopstal@gmail.com>
2018-04-04 17:25:22 +0200

    fix: individual pipes not publicly available

--- src/collection/collection.module.ts
+++ src/collection/collection.module.ts
@@ -34,40 +34,6 @@ import {UniqPipe} from './uniq.pipe';
 import {WherePipe} from './where.pipe';
 import {XORPipe} from './xor.pipe';
 
-export {AfterWherePipe} from './after-where.pipe';
-export {AfterPipe} from './after.pipe';
-export {BeforeWherePipe} from './before-where.pipe';
-export {BeforePipe} from './before.pipe';
-export {ChunkByPipe} from './chunk-by.pipe';
-export {ConcatPipe} from './concat.pipe';
-export {ContainsPipe} from './contains.pipe';
-export {CountByPipe} from './count-by.pipe';
-export {DefaultsPipe} from './defaults.pipe';
-export {EveryPipe} from './every.pipe';
-export {FilterByPipe} from './filter-by.pipe';
-export {FirstPipe} from './first.pipe';
-export {FlattenPipe} from './flatten.pipe';
-export {FuzzyByPipe} from './fuzzy-by.pipe';
-export {FuzzyPipe} from './fuzzy.pipe';
-export {GroupByPipe} from './group-by.pipe';
-export {IsEmptyPipe} from './is-empty.pipe';
-export {JoinPipe} from './join.pipe';
-export {LastPipe} from './last.pipe';
-export {MapPipe} from './map.pipe';
-export {MeanPipe} from './mean.pipe';
-export {OmitPipe} from './omit.pipe';
-export {OrderByPipe} from './order-by.pipe';
-export {PickPipe} from './pick.pipe';
-export {RangePipe} from './range.pipe';
-export {RemoveWithPipe} from './remove-with.pipe';
-export {RemovePipe} from './remove.pipe';
-export {ReversePipe} from './reverse.pipe';
-export {SearchFieldPipe} from './search-field.pipe';
-export {ToArrayPipe} from './to-array.pipe';
-export {UniqPipe} from './uniq.pipe';
-export {WherePipe} from './where.pipe';
-export {XORPipe} from './xor.pipe';
-
 @NgModule({
   declarations: [
     AfterWherePipe, BeforeWherePipe, AfterPipe,       BeforePipe,  ChunkByPipe,  ConcatPipe,
--- src/ng-pipes.module.ts
+++ src/ng-pipes.module.ts
@@ -6,6 +6,12 @@ import {MathPipesModule} from './math/math.module';
 import {ObjectPipesModule} from './object/object.module';
 import {StringPipesModule} from './string/string.module';
 
+export {BooleanPipesModule} from './boolean/boolean.module';
+export {CollectionPipesModule} from './collection/collection.module';
+export {MathPipesModule} from './math/math.module';
+export {ObjectPipesModule} from './object/object.module';
+export {StringPipesModule} from './string/string.module';
+
 @NgModule({
   exports: [
     BooleanPipesModule, CollectionPipesModule, MathPipesModule, ObjectPipesModule, StringPipesModule
